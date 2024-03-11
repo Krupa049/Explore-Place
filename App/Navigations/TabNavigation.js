fb9f710012cbd5be7f8044737699e8ecd582da82
Krupa049 <golisaikrupa.409@gmail.com>
2024-03-10 20:53:40 -0400

    Setup

---
+++ App/Navigations/TabNavigation.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+export default function TabNavigation() {
+    const Tab = createBottomTabNavigator();
+    return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  )
+}
